8270bafd9ecfe8a3ac918c08727de178af88a6de
U390709076 <90101405+elessar99@users.noreply.github.com>
2022-10-09 15:26:35 +0300

    .

--- src/views/AnimeViews/AnimeComponent/Food.js
+++ src/views/AnimeViews/AnimeComponent/Food.js
@@ -1,8 +1,39 @@
+import "./Css/FoodAnime.css"
+
 const FoodAnime = () =>{
   return (
     <>
-    <div>
-      food
+    <div className="aFoodPage">
+      <div className="aFoodOne">
+        <div className="aFoodHeader">Anime Nedir</div>
+        <hr color="blue" width="100%" />
+        <div>Anime (Japonca: アニメ), Japonya'ya özgü manga çizim sanatıyla çizilmiş çizgi film, animasyondur. Anime kelimesi İngilizcedeki animation kelimesinin kısaltılmış halidir. Yetişkinlere göre olan yapımların konusu bir sinema filmi gibi ciddi olur. TDK tarafından "Japon çizgi filmi" karşılığı teklif edilmiştir.</div>
+      </div>
+      <div className="aFoodTwo">
+        <div className="aFoodHeader">Biz Kimiz?</div>
+        <hr color="red" width="100%" />
+        <div>
+          <div>Hakkımızda</div>
+          <div>E-Mail</div>
+          <div>Discord</div>
+          <div style={{display:"flex"}}>
+            <div style={{marginRight:"2em"}}> I </div>
+            <div style={{marginRight:"2em"}}> Y </div>
+            <div style={{marginRight:"2em"}}> F </div>
+          </div>
+        </div>
+      </div>
+      <div className="aFoodThree">
+        <div className="aFoodHeader">Destek</div>
+        <hr  color="green" width="100%" />
+        <div>
+          <div>SSS</div>
+          <div>Hata Bildir</div>
+          <div>Canlı Destek</div>
+          <div>Ekibe Başvur</div>
+        </div>
+      </div>
+
     </div>
     </>
   );
