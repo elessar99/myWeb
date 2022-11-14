68eafd93e6327ddffacd04b70d7564b7952f7594
U390709076 <90101405+elessar99@users.noreply.github.com>
2022-11-14 11:25:47 +0300

    .

--- src/views/AnimeViews/AnimeComponent/AnimeTakvim.js
+++ src/views/AnimeViews/AnimeComponent/AnimeTakvim.js
@@ -3,7 +3,76 @@ import "./Css/Takvim.css"
 const AnimeTakvim = () =>{
     return (
       <>
-      <div className="fullTakvim">takvim</div>
+      <div className="aTakvimHead">Takvim</div>
+      <div className="fullTakvim">
+        <div className="takvimDays">
+          <div className="takvimDay">
+          pazartesi
+          </div>
+          <div className="takvimAnime">
+            Naruto - 20:00
+          </div>
+          <div className="takvimAnime">
+            Dororo - 13:00
+          </div>
+          <div className="takvimDay">
+          salı
+          </div>
+          <div className="takvimAnime">
+            The Misfit of Demon King Academy - 17:30
+          </div>
+          <div className="takvimAnime">
+            
+          </div>
+          <div className="takvimDay">
+          carşamba
+          </div>
+          <div className="takvimAnime">
+            
+          </div>
+          <div className="takvimAnime">
+            
+          </div>
+          <div className="takvimDay">
+          perşembe
+          </div>
+          <div className="takvimAnime">
+            
+          </div>
+          <div className="takvimAnime">
+            
+          </div>
+          <div className="takvimDay">
+          cuma
+          </div>
+          <div className="takvimAnime">
+            
+          </div>
+          <div className="takvimAnime">
+            
+          </div>
+          <div className="takvimDay">
+          cumartesi
+          </div>
+          <div className="takvimAnime">
+            
+          </div>
+          <div className="takvimAnime">
+            
+          </div>
+          <div className="takvimDay">
+          pazar
+          </div>
+          <div className="takvimAnime">
+            
+          </div>
+          <div className="takvimAnime">
+            
+          </div>
+        </div>
+        
+        
+      </div>
       </>
     );
   }
